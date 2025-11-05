d86e0ed173bddf5ccb98bff0adb5d79c899e94ac
jinxin0211 <jinxin1355@gmail.com>
2025-11-05 16:13:25 +0800

    1105

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var server = express();
+var bodyParser = require("body-parser");
+
+server.use(express.static(__dirname + "/Public"));
+server.use(bodyParser.urlencoded());
+
+server.get("/", (req,res)=>{
+    res.send("Hello world!");
+})
+
+server.get("/services", (req,res)=>{
+    var Services = [
+        {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
+        {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+        {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
+     ];
+     res.send(Services);
+})
+
+server.get("/about", (req,res)=>{
+    res.send("welcome"+req.query.user+"My first NodeJS server!");
+})
+
+server.listen(80)
